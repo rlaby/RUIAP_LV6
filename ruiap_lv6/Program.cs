c19da282a7018d9713c1ace73addc9eeefab57ee
Student <rlaby5@gmail.com>
2016-05-24 18:30:42 +0200

    final

--- ruiap_lv6/Program.cs
+++ ruiap_lv6/Program.cs
@@ -41,7 +41,7 @@ namespace CallRequestResponseService
                             new StringTable() 
                             {
                                 ColumnNames = new string[] {"MPG", "Cyl", "Displacement", "Horsepower", "Weight", "Acceleration", "Year", "CountryCode", "Model"},
-                                Values = new string[,] {  { "0", "0", "0", "0", "0", "0", "0", "0", "value" },  { "0", "0", "0", "0", "0", "0", "0", "0", "value" },  }
+                                Values = new string[,] {  { "18", "8", "307", "130", "3504", "12", "70", "1", "malibu" },  { "15", "8", "350", "165", "3693", "11.5", "70", "1", "skylarc 320" },  }
                             }
                         },
                     },
